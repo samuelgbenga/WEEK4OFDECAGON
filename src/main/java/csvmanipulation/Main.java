4e11e03cda3fc6799a596d7c690724a10e6402a6
samuelgbenga <samuelgbenga972@gmail.com>
2024-05-12 02:58:33 +0100

    crazy manipulation of comma sperated values csv

---
+++ src/main/java/csvmanipulation/Main.java
@@ -0,0 +1,4 @@
+package csvmanipulation;
+
+public class Main {
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,63 @@
 package org.example;
 
+import java.io.*;
+
+import static java.lang.Integer.parseInt;
+
+
+
+
 public class Main {
+
+   static void toRead(String filePath){
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+            String line = reader.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
+            // line = reader.readLine();
+
+            reader.close();
+        }catch  (Exception e){
+            System.out.println("it is wrong");}
+
+    }
+
+    // to write file
+    static void toWrite(String pathToWrite){
+       try {
+           BufferedWriter writer = new BufferedWriter(new FileWriter(pathToWrite));
+           writer.write("write a string");
+           writer.close();
+       }catch (Exception e){
+           System.out.println("we don't know the exception yet");
+       }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       String readFilePath = "file.txt";
+        toRead(readFilePath);
+
+        String writeFilePath = "output.txt";
+        toWrite(writeFilePath);
+
+        String writeToCSV = "output1.csv";
+        toWrite(writeToCSV);
+       /*
+       to write your file on other path you need to specify the absolute
+       path:
+       eg:
+       driveName:\\dir\\dir\\filename with extension.
+        */
+
+
+
     }
 }
